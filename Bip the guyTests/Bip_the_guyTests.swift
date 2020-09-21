af24dfa6fbdebef613c05e47a7f3be69731fa5ec
Alex Golden <goldenah@bc.edu>
2020-09-21 11:38:15 -0400

    Initial Commit

---
+++ Bip the guyTests/Bip_the_guyTests.swift
@@ -0,0 +1,33 @@
+//
+//  Bip_the_guyTests.swift
+//  Bip the guyTests
+//
+//  Created by Alex Golden on 9/21/20.
+//
+
+import XCTest
+@testable import Bip_the_guy
+
+class Bip_the_guyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
